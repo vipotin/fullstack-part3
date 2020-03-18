65a5d316c4a7f0502fc163b3f0c7c74c9f1696cc
vipotin <vilma.pohjonen@gmail.com>
2020-03-18 12:06:47 +0200

    uibuild

---
+++ build/precache-manifest.12e213ed9aa4783bd62513ad53f0d959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1d66ce0cdf377ff6a2add5d222c3b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b542a23122ad972822e6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "015eca964e9db1299b2b",
+    "url": "/static/js/2.65deedc6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.65deedc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b542a23122ad972822e6",
+    "url": "/static/js/main.3414c629.chunk.js"
+  },
+  {
+    "revision": "57e3d8805ca2bcfd1d13",
+    "url": "/static/js/runtime-main.fb40897c.js"
+  }
+]);
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const loggerFormat = ':method :url :status :res[content-length] - :response-time
 app.use(express.json())
 app.use(morgan(loggerFormat))
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('data', function(req, res) {
   if (req.method === "POST") return JSON.stringify(req.body)
